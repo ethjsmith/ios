21dedcf8e3a27b267b479da516c441a6c4c22967
ejsmith <ethan@esmithy.net>
2022-01-21 15:49:32 -0700

    jan21

--- memrize/ContentView.swift
+++ memrize/ContentView.swift
@@ -9,18 +9,30 @@ import SwiftUI
 
 
 struct ContentView: View {
-    var txts = ["hit","me","with a","bus"]
+    var txts = ["😅1","😅2","😅3","😅4","😅5","😅6","😅7","😅8","😅9","😅0","😅11","😅"]
+    @State var cnt = 4
     var body: some View {
-        HStack{
-            
-            cardView(faceup:false,txt:txts[0])
-            cardView(faceup:false,txt:txts[1])
-            cardView(faceup:false,txt: txts[2])
-            cardView(txt :txts[3])
+        VStack{
+            LazyVGrid(columns:[GridItem(),GridItem(),GridItem()]){
+        //HStack{
+            ForEach(txts[0..<cnt],id: \.self) { t in
+                cardView(txt:t).aspectRatio(2/3, contentMode: .fit)
+            }
+//            cardView(faceup:false,txt:txts[0])
+//            cardView(faceup:false,txt:txts[1])
+//            cardView(faceup:false,txt: txts[2])
+//            cardView(txt :txts[3])
 
         }.padding(.horizontal)
+        Spacer()
+            HStack{
+        Button(action:{cnt += 1},label:{ Image(systemName:"plus.circle")})
+        Spacer()
+        Button(action:{cnt -= 1},label:{ Image(systemName:"minus.circle").foregroundColor(.red)})
+            }.padding()
+        }.padding(.horizontal)
+        }
     }
-}
 
 
 
